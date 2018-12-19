3a5babee854ddf101065a96dc356a081a4c94d91
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-19 23:47:47 +0000

    Added two error conditions

--- push.c
+++ push.c
@@ -4,32 +4,31 @@
  *push - push new node to the end of the stack
  *@stack: double pointer to the head of the stack
  *@line_number: number of line in the file
- *
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp;
+	stack_t *new;
 	char *num;
 
 	num = strtok(NULL, DELIMS);
-	if (num == NULL)
+	if (num == NULL || *stack == NULL || line_number == 0)
 	{
 		printf("L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	tmp = malloc(sizeof(stack_t));
-	if (tmp == NULL)
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
 	{
 		printf("Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	tmp->n = atoi(num);
-	tmp->prev = NULL;
-	tmp->next = *stack;
+	new->n = atoi(num);
+	new->prev = NULL;
+	new->next = *stack;
 
 	if (*stack != NULL)
-		(*stack)->prev = tmp;
+		(*stack)->prev = new;
 
-	*stack = tmp;
+	*stack = new;
 }
