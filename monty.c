270700f9f926d756b69189d8cb39918ca845baa8
Fernando Gonzalez-Morales <542@holbertonschool.com>
2018-12-20 17:29:39 +0000

    exec_line_ops tests pushing to the global stack and the try test script now uses pall.c

--- monty.c
+++ monty.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 
-state_t global;
-void exec_line_ops(char *line);
+state_t global = {0, 0, NULL};
+void exec_line_ops(char *line, size_t line_num);
 
 int main(int argc, char** argv)
 {
 	FILE *file = NULL;
 	char *line = NULL;
-	size_t len = 0;
+	size_t len = 0, line_num = 0;
 
 	/* Check that exactly one file argument was given */
 	if (argc != 2)
@@ -25,28 +25,40 @@ int main(int argc, char** argv)
 	}
 
 	/* read every line in file and try executing it */
-	global.line_number = 0;
 	while (getline(&line, &len, file) != -1)
-	{
-		global.line_number += 1;
 		/* execute any valid commands on line */
-		exec_line_ops(line);
-	}
+		exec_line_ops(line, ++line_num);
 
-return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
-void exec_line_ops(char *line)
+void exec_line_ops(char *line, size_t line_num)
 {
 	char *cmd = NULL, *arg = NULL;
 
 	cmd = strtok(line, DELIMS);
 	arg = strtok(NULL, DELIMS);
 	if (strcmp(cmd, "push") == 0)
-		printf("Pushing %d\n", atoi(arg));
+	{
+		stack_t *new = malloc(sizeof(new));
+		if (!new)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_SUCCESS);
+		}
+		new->n = atoi(arg);
+		new->prev = NULL;
+		new->next = global.stack;
+		if (global.stack)
+			global.stack->prev = new;
+		global.stack = new;
+		printf("Pushing %d\n", global.stack->n);
+	}
+	else if (strcmp(cmd, "pall") == 0)
+		pall(&global.stack, line_num);
 	else
 	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", global.line_number, cmd);
+		fprintf(stderr, "L%d: unknown instruction %s\n", (int)line_num, cmd);
 		exit(EXIT_FAILURE);
 	}
 }
